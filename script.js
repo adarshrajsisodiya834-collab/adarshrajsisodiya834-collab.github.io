5d392ee2a528f590f1339b374976918c90183b37
Adarshraj Singh Sisodiya <adarshrajsisodiya834@gmail.com>
2026-07-24 17:14:44 +0530

    Update Portfolio

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ document.querySelectorAll("section").forEach(section=>{
     section.classList.add("hidden");
     observer.observe(section);
 });
+
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
